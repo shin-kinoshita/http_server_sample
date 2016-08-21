0a91162b73af41e0dc2f60c7cf031bff5e60f1b3
shin-kinoshita <shin.kinoshita@nextremer.com>
2016-08-21 17:17:11 +0530

    Implemeted post function of saving image

--- post_image.py
+++ post_image.py
@@ -1,6 +1,7 @@
 from BaseHTTPServer import BaseHTTPRequestHandler
 import cgi
 from PIL import Image
+import StringIO
 
 class PostHandler(BaseHTTPRequestHandler):
     
@@ -23,26 +24,31 @@ class PostHandler(BaseHTTPRequestHandler):
         self.wfile.write('Path: %s\n' % self.path)
         self.wfile.write('Form data:\n')
 
+        name = ""
+
         # Echo back information about what was posted in the form
         for field in form.keys():
             if field == 'image':
                 name = form[field].filename
-                print str(form[field])
-                img = self._get_image(form[field])
-                img.save(image_dir_path + "/" + name)
+                image_item = form[field]
+
+        img = get_image(image_item)
+        img.save(self.image_dir_path + "/" + name)
+        print "Saved an image"
+        print "file name: " + name
         return
 
-    def _get_image(self, image_item):
+def get_image(image_item):
 
-        raw_data = image_item.file.read()
-        raw_len = len(raw_data)
-        img = Image.frombytes('RGB', raw_len, raw_data)
-        return img
+    raw_data = image_item.file.read()
+    img = Image.open(StringIO.StringIO(raw_data))
+    return img
 
 
 if __name__ == '__main__':
     from BaseHTTPServer import HTTPServer
-    server = HTTPServer(('localhost', 8080), PostHandler)
+    #server = HTTPServer(('localhost', 8080), PostHandler)
+    server = HTTPServer(('192.168.1.2', 8080), PostHandler)
     print 'Starting server, use <Ctrl-C> to stop'
     server.serve_forever()
     
